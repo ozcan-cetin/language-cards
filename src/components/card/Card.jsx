8c3699203c847833ce60fc5fb5e617d5e4eaebee
özcan çetin <cetin445@gmail.com>
2022-06-29 21:33:32 +0300

    item css

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,18 @@
-
+import {languages} from "../../helpers/data";
+import Item from "../item/Item";
 
 const Card = () => {
   return (
-    <div></div>
+    <div className="cards-area-container">
+        <div className="bars"></div>
+        <h1 className="language-title">Languages</h1>
+        <div className="cards-container">
+            {/* {console.log(languages)} */}
+            {languages.map((item, index)=>{
+                return <Item card={item}/>
+            })}
+        </div>
+    </div>
   )
 }
 
